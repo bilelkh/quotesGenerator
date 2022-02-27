0c4df82f438c1263033c30a6c5bf07701077e4f8
Ines-Bouguerra <54783558+Ines-Bouguerra@users.noreply.github.com>
2022-02-27 22:06:22 +0100

    Fix Project Style

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
+
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import SoftUIDashboard from "./soft-ui-dashboard";
+
+
 
 const appInstance = createApp(App);
+appInstance.use(store);
 appInstance.use(router);
+appInstance.use(SoftUIDashboard);
 appInstance.mount("#app");
